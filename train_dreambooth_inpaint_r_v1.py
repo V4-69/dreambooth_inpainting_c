08f01d89366b3de0448c90ca39f8b1c7a6f7f133
Влад Влад <vladik208@gmail.com>
2023-01-10 18:00:17 +0300

    c2

--- train_dreambooth_inpaint_r_v1.py
+++ train_dreambooth_inpaint_r_v1.py
@@ -38,19 +38,19 @@ from transformers import CLIPTextModel, CLIPTokenizer
 logger = get_logger(__name__)
 
 
-def prepare_mask_and_masked_image(image, mask):
+def prepare_mask_and_masked_image(image, mask, threshold=0.3):
     image = np.array(image.convert("RGB"))
     image = image[None].transpose(0, 3, 1, 2)
-    image = torch.from_numpy(image).to(dtype=torch.float32) / 127.5 - 1.0
+    image = torch.from_numpy(image).to(dtype=torch.float32) / 255.0 
 
     mask = np.array(mask.convert("L"))
     mask = mask.astype(np.float32) / 255.0
     mask = mask[None, None]
-    mask[mask < 0.5] = 0
-    mask[mask >= 0.5] = 1
+    mask[mask < threshold] = 0
+    mask[mask >= threshold] = 1
     mask = torch.from_numpy(mask)
 
-    masked_image = image * (mask < 0.5)
+    masked_image = image * (mask < threshold)
 
     return mask, masked_image
 
